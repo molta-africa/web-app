1235039ca52f936025df6597f28809fd0d232f3e
Caleb Nwaizu <user@users-MacBook-Pro.local>
2025-05-19 08:49:32 +0100

    Improve idle timeout tracking and persist activity timestamp
    - Add support for touch events and debounce user activity
    - Persist last activity timestamp in localStorage
    - Restore session timeout after tab visibility changes
    -

--- src/app/home/timeout-dialog/idle-timeout.service.ts
+++ src/app/home/timeout-dialog/idle-timeout.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Logger } from 'app/core/logger/logger.service';
 import { environment } from 'environments/environment';
 import { interval, merge, fromEvent, Observable } from 'rxjs';
-import { takeUntil, repeat, map } from 'rxjs/operators';
+import { takeUntil, repeat, map, filter, tap, debounceTime } from 'rxjs/operators';
 
 /**
  *  Idle timeout service used to track idle user
@@ -10,6 +11,9 @@ import { takeUntil, repeat, map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class IdleTimeoutService {
+  private readonly logger: Logger = new Logger('Idle Timeout');
+  // Constants
+  private readonly STORAGE_NAME = 'user_last_activity_timestamp';
   // max timeout for an idle user
   readonly timeoutDelay = environment.session.timeout.idleTimeout || 300000; // 5 minutes
 
@@ -22,13 +26,73 @@ export class IdleTimeoutService {
       'keydown',
       'wheel',
       'mousedown',
-      'scroll'
+      'scroll',
+      'touchstart',
+      'touchmove',
+      'touchend',
+      'touchcancel'
     ];
-    const $signal = merge(...events.map((eventName) => fromEvent(document, eventName)));
-    this.$onSessionTimeout = interval(this.timeoutDelay).pipe(
-      takeUntil($signal),
+    const $userActivity = merge(...events.map((eventName) => fromEvent(document, eventName))).pipe(
+      debounceTime(10),
+      tap(() => {
+        this.updateLastActivity();
+      })
+    );
+
+    const userEvents = interval(this.timeoutDelay).pipe(
+      takeUntil($userActivity),
       map(() => undefined),
       repeat()
     );
+
+    this.$onSessionTimeout = merge(
+      userEvents,
+
+      // When window is focused
+      fromEvent(document, 'visibilitychange').pipe(
+        filter(() => document.visibilityState === 'visible'),
+        map(() => {
+          if (this.hasTimedOut()) {
+            return undefined;
+          } else {
+            return null;
+          }
+        }),
+        filter((value) => value !== null)
+      )
+    );
+  }
+
+  /**
+   * Checks for if the inactive period exceeds the allowed timeout
+   */
+  private hasTimedOut(): boolean {
+    const lastActivity = this.getLastActivity();
+    const now = Date.now();
+    const inactive = now - lastActivity;
+    return this.timeoutDelay <= inactive;
+  }
+
+  /**
+   * Gets the last recorded activity
+   */
+  private getLastActivity() {
+    try {
+      const activity = localStorage.getItem(this.STORAGE_NAME);
+      return parseInt(activity);
+    } catch {
+      return Date.now();
+    }
+  }
+
+  /**
+   * Update the last activity
+   */
+  private updateLastActivity() {
+    try {
+      localStorage.setItem(this.STORAGE_NAME, Date.now().toString());
+    } catch {
+      this.logger.error('Failed to save timestamp to localStorage');
+    }
   }
 }
